6b5cbc4440357449ed400c17d1cdd460d60ba95f
Ana Hernandez <her17138@uvg.edu.gt>
2019-02-25 23:34:20 -0600

    finalizacion de querys
    hay que revisar la 10 porque no estoy segura que tire lo que queremos que tire

--- proyecto1.sql
+++ proyecto1.sql
@@ -53,6 +53,7 @@ JOIN unique_carriers U ON U.code = ontime.Reporting_Airline
 WHERE arrDelay>0
 GROUP BY Reporting_Airline 
 ORDER BY PorcentajeRetraso ASC
+LIMIT 5;
 
 /*
     PREGUNTA 5
@@ -86,8 +87,62 @@ ORDER BY Razon ASC
     PREGUNTA 7
  */
 
+SELECT
+    Reporting_Airline,
+    ((((SUM(CASE WHEN Quarter = 2 and "Year" = '2017' NOT NULL THEN 1 ELSE 0 END) - SUM(CASE WHEN Quarter = 1 and "Year" = '2017' NOT NULL THEN 1 ELSE 0 END))*1.0/SUM(CASE WHEN Quarter = 1 and "Year" = '2017' NOT NULL THEN 1 ELSE 0 END)) + 
+    ((SUM(CASE WHEN Quarter = 3 and "Year" = '2017' NOT NULL THEN 1 ELSE 0 END) - SUM(CASE WHEN Quarter = 2 and "Year" = '2017' NOT NULL THEN 1 ELSE 0 END))*1.0/SUM(CASE WHEN Quarter = 2 and "Year" = '2017' NOT NULL THEN 1 ELSE 0 END)) +
+    ((SUM(CASE WHEN Quarter = 4 and "Year" = '2017' NOT NULL THEN 1 ELSE 0 END) - SUM(CASE WHEN Quarter = 3 and "Year" = '2017' NOT NULL THEN 1 ELSE 0 END))*1.0/SUM(CASE WHEN Quarter = 3 and "Year" = '2017' NOT NULL THEN 1 ELSE 0 END)) + 
+    ((SUM(CASE WHEN Quarter = 1 and "Year" = '2018' NOT NULL THEN 1 ELSE 0 END) - SUM(CASE WHEN Quarter = 4 and "Year" = '2017' NOT NULL THEN 1 ELSE 0 END))*1.0/SUM(CASE WHEN Quarter = 4 and "Year" = '2017' NOT NULL THEN 1 ELSE 0 END)) +
+    ((SUM(CASE WHEN Quarter = 2 AND "Year" = '2018' NOT NULL THEN 1 ELSE 0 END) - SUM(CASE WHEN Quarter = 1 AND "Year" = '2018' NOT NULL THEN 1 ELSE 0 END))*1.0/SUM(CASE WHEN Quarter = 1 AND "Year" = '2018' NOT NULL THEN 1 ELSE 0 END)) +
+    ((SUM(CASE WHEN Quarter = 3 AND "Year" = '2018' NOT NULL THEN 1 ELSE 0 END) - SUM(CASE WHEN Quarter = 2 AND "Year" = '2018' NOT NULL THEN 1 ELSE 0 END))*1.0/SUM(CASE WHEN Quarter = 2 AND "Year" = '2018' NOT NULL THEN 1 ELSE 0 END)) +
+    ((SUM(CASE WHEN Quarter = 4 AND "Year" = '2018' NOT NULL THEN 1 ELSE 0 END) - SUM(CASE WHEN Quarter = 3 AND "Year" = '2018' NOT NULL THEN 1 ELSE 0 END))*1.0/SUM(CASE WHEN Quarter = 3 AND "Year" = '2018' NOT NULL THEN 1 ELSE 0 END)))/4)*100 as CrecimientoPromedio
+FROM ontime ontime_externo
+GROUP BY Reporting_Airline
+ORDER BY CrecimientoPromedio DESC;
 
 
 /*
     PREGUNTA 8
  */
+
+SELECT Reporting_Airline, COUNT(DISTINCT origen) as estados
+FROM ontime o
+JOIN (
+    SELECT COUNT(DISTINCT DestAirportID) as CantAeropuertos, OriginStateNm as origen
+    FROM ontime
+    GROUP BY OriginStateNm
+    ORDER BY CantAeropuertos DESC
+    LIMIT 10
+) tabla ON o.DestStateNm = tabla.origen
+GROUP BY Reporting_Airline
+ORDER BY estados DESC
+
+/*
+    PREGUNTA 9
+ */
+
+SELECT Reporting_Airline, RetrasosAerolineas, SUM(Distance)/(SUM(AirTime)/60) as Velocidad
+FROM ontime
+JOIN (
+    SELECT SUM(arrDelay) as RetrasosAerolineas, Reporting_Airline as aero
+    FROM ontime 
+    WHERE arrDelay>0
+    GROUP BY Reporting_Airline
+    ORDER BY RetrasosAerolineas ASC
+) Delays ON ontime.Reporting_Airline = Delays.aero
+GROUP BY Reporting_Airline
+ORDER BY Velocidad DESC
+ /*
+    PREGUNTA 10
+ */
+
+SELECT Reporting_Airline, Clasificacion, TiempoEnAire
+FROM ontime
+JOIN (
+    SELECT SUM(AirTime) as TiempoEnAire, DistanceGroup as Clasificacion
+    FROM ontime 
+    GROUP BY DistanceGroup
+    ORDER BY TiempoEnAire ASC
+) TiemposAire ON ontime.DistanceGroup = TiemposAire.Clasificacion
+GROUP BY Reporting_Airline
+ORDER BY TiempoEnAire, DistanceGroup ASC
